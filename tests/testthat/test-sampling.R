f339817a7791588761a6d2c9aa7ffede8ec665f1
jtimonen <juho.timonen@iki.fi>
2021-12-23 15:53:13 +0200

    add get_array_draw() and improve test coverage

--- tests/testthat/test-sampling.R
+++ tests/testthat/test-sampling.R
@@ -1,9 +1,11 @@
 a <- example_odemodel(compile = F)
 a$reinit()
 
-test_that("prior can be used to simulate data", {
+test_that("prior and posterior sampling works", {
   num_tp <- 15
   G <- 3
+  t0 <- 0
+  t <- c(1:num_tp)
   dat <- list(
     G = G,
     D = 2 * G,
@@ -15,14 +17,32 @@ test_that("prior can be used to simulate data", {
   )
   fit <- sample_odemodel(
     model = a,
-    t0 = 0, t = 1:num_tp,
+    t0 = t0, t = t,
     data = dat,
     prior_only = TRUE,
     iter_warmup = 1000, iter_sampling = 1000, chains = 1, refresh = 0
   )
-  x_ode <- fit$draws("x_ode")
-  I_gen <- fit$draws("I_gen")
+
+  idx <- 322
+  x_ode <- get_array_draw(fit, variable = "x_ode", iteration = idx)
+  I_gen <- get_array_draw(fit, variable = "I_gen", iteration = idx)
   expect_true(is(fit, "CmdStanMCMC"))
-  expect_equal(dim(x_ode), c(1000, 1, 90)) # 90 = 15 * 6
-  expect_equal(dim(I_gen), c(1000, 1, 45)) # 45 = 15 * 3
+  expect_equal(dim(x_ode), c(15, 6))
+  expect_equal(dim(I_gen), c(15, 3))
+
+  # Posterior sampling
+  dat$I_data <- I_gen
+  post_fit <- sample_odemodel(
+    model = a,
+    t0 = t0, t = t,
+    data = dat,
+    iter_warmup = 10, iter_sampling = 10, chains = 1, refresh = 0
+  )
+
+  idx <- 2
+  x_ode <- get_array_draw(post_fit, variable = "x_ode", iteration = idx)
+  I_gen <- get_array_draw(post_fit, variable = "I_gen", iteration = idx)
+  expect_true(is(post_fit, "CmdStanMCMC"))
+  expect_equal(dim(x_ode), c(15, 6))
+  expect_equal(dim(I_gen), c(15, 3))
 })
